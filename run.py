b2c7468d058942889695e85c990a6893d08ea533
hutli <jensstaermose@hotmail.com>
2021-08-16 21:00:43 +0200

    nametag website

--- run.py
+++ run.py
@@ -6,22 +6,16 @@ from PIL import Image, ImageFont, ImageDraw
 from font_hanken_grotesk import HankenGroteskBold, HankenGroteskMedium
 from font_intuitive import Intuitive
 from inky.auto import auto
+from fastapi import FastAPI, Response, status
+from fastapi.responses import PlainTextResponse, HTMLResponse
 
 
-print("""Inky pHAT/wHAT: Hello... my name is:
-
-Use Inky pHAT/wHAT as a personalised name badge!
-
-""")
+app = FastAPI()
 
 try:
     inky_display = auto(ask_user=True, verbose=True)
 except TypeError:
     raise TypeError("You need to update the Inky library to >= v1.1.0")
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--name', '-n', type=str, required=True, help="Your name")
-args, _ = parser.parse_known_args()
 
 # inky_display.set_rotation(180)
 try:
@@ -29,46 +23,20 @@ try:
 except NotImplementedError:
     pass
 
-# Figure out scaling for display size
-
-scale_size = 1.0
-padding = 0
-
-if inky_display.resolution == (400, 300):
-    scale_size = 2.20
-    padding = 15
-
-if inky_display.resolution == (600, 448):
-    scale_size = 2.20
-    padding = 30
-
-if inky_display.resolution == (250, 122):
-    scale_size = 1.30
-    padding = -5
-
 # Create a new canvas to draw on
-
 img = Image.new("P", inky_display.resolution)
 draw = ImageDraw.Draw(img)
 
 # Load the fonts
-
-intuitive_font = ImageFont.truetype(Intuitive, int(22 * scale_size))
-hanken_bold_font = ImageFont.truetype(HankenGroteskBold, int(35 * scale_size))
-hanken_medium_font = ImageFont.truetype(
-    HankenGroteskMedium, int(16 * scale_size))
-
-# Grab the name to be displayed
-
-name = args.name
+intuitive_font = ImageFont.truetype(Intuitive, 30)
+hanken_bold_font = ImageFont.truetype(HankenGroteskBold, 35)
+hanken_medium_font = ImageFont.truetype(HankenGroteskMedium, 16)
 
 # Top and bottom y-coordinates for the white strip
-
 y_top = int(inky_display.height * (5.0 / 10.0))
 y_bottom = y_top + int(inky_display.height * (4.0 / 10.0))
 
 # Draw the red, white, and red strips
-
 for y in range(0, y_top):
     for x in range(0, inky_display.width):
         img.putpixel((x, y), inky_display.BLACK if inky_display.colour ==
@@ -83,30 +51,65 @@ for y in range(y_bottom, inky_display.height):
         img.putpixel((x, y), inky_display.BLACK if inky_display.colour ==
                      "black" else inky_display.RED)
 
-# Calculate the positioning and draw the "Hello" text
 
+# Calculate the positioning and draw the "Hello" text
 hello_w, hello_h = hanken_bold_font.getsize("Hello")
 hello_x = int((inky_display.width - hello_w) / 2)
-hello_y = 0 + padding
+hello_y = 0
 draw.text((hello_x, hello_y), "Hello",
           inky_display.WHITE, font=hanken_bold_font)
 
 # Calculate the positioning and draw the "my name is" text
-
 mynameis_w, mynameis_h = hanken_medium_font.getsize("my name is")
 mynameis_x = int((inky_display.width - mynameis_w) / 2)
-mynameis_y = hello_h + padding
+mynameis_y = hello_h
 draw.text((mynameis_x, mynameis_y), "my name is",
           inky_display.WHITE, font=hanken_medium_font)
 
-# Calculate the positioning and draw the name text
-
-name_w, name_h = intuitive_font.getsize(name)
-name_x = int((inky_display.width - name_w) / 2)
-name_y = int(y_top + ((y_bottom - y_top - name_h) / 2))
-draw.text((name_x, name_y), name, inky_display.BLACK, font=intuitive_font)
-
-# Display the completed name badge
 
 inky_display.set_image(img)
 inky_display.show()
+
+
+@app.get('/', response_class=HTMLResponse)
+def index():
+    return '''
+    <script type="text/javascript">
+    var input;
+    var output;
+    function onload() { 
+        input = document.getElementById('input');
+        output = document.getElementById('output');
+    }
+    function set_name(){
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                output.innerHTML = xhr.response.replace(/\\r?\\n/g, '<br/>');
+            }
+        }
+        xhr.open('get', '/nametag/' + input.value, true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+        xhr.send();
+    }
+    </script>
+
+    <body onload="onload();">
+        <input type="text" name="enter" class="enter" value="" id="input"/>
+        <input type="button" value="click" onclick="set_name();"/>
+        <p id="output"></p>
+    </body>
+    '''
+
+
+@app.get("/nametag/{name}", status_code=200)
+def read_item(name: str):
+    # Calculate the positioning and draw the name text
+    name_w, name_h = intuitive_font.getsize(name)
+    name_x = int((inky_display.width - name_w) / 2)
+    name_y = int(y_top + ((y_bottom - y_top - name_h) / 2))
+    draw.text((name_x, name_y), name, inky_display.BLACK, font=intuitive_font)
+
+    # Display the completed name badge
+    inky_display.set_image(img)
+    inky_display.show()
